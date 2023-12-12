c5e41fe6c0191e29f812a799a4012a28eff7c189
TobyJWalker <tobyjwalker03@outlook.com>
2023-12-12 16:36:17 +0000

    post request handler

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, Response
 import requests
+import json
 
 app = Flask(__name__)
 SITE_NAME = 'http://18.133.31.185/'
@@ -17,6 +18,11 @@ VALID = {
                 'staffs/<id>', 
                 'patients/<id>', 
                 'notes/<id>'],
+    
+    'POST' : ['hospitals',
+            'staffs',
+            'patients', 'patients/<id>/screen'
+            'notes'],
 }
 
 # validate the Get method path
@@ -67,6 +73,63 @@ def validate_delete_path(path):
     
     return True
 
+# validate the Post method path
+def validate_post_path(path):
+    # split path
+    path = path.split('/')
+
+    # check if length is not 1
+    if len(path) != 1 and len(path) != 3:
+        return False
+    
+    # check 3 length path is valid]
+    if len(path) == 3 and (path[0] != 'patients' or path[2] != 'screen' or not path[1].isdigit()):
+        return False
+    
+    # check if first part is valid category
+    if path[0] not in VALID['CATEGORIES']:
+        return False
+    
+    return True
+
+# validate post content
+def validate_post_content(content, path):
+    # check if content is json
+    try:
+        data = json.loads(content)
+    except:
+        return 406
+    
+    # get the object type to make
+    path = path.split('/')
+    obj = path[0]
+
+    # check content matches object
+    if obj == 'hospitals':
+        if not 'name' in data.keys():
+            return 400
+        
+    elif obj == 'patients':
+        if len(path) == 1:
+            if not all(key in data.keys() for key in ['name', 'hospital_id', 'birthdate']):
+                return 400
+        else:
+            if not 'images' in data.keys():
+                return 400
+    
+    elif obj == 'staffs':
+        if not all(key in data.keys() for key in ['name', 'hospital_id', 'password']):
+            return 400
+    
+    elif obj == 'notes':
+        if not all(key in data.keys() for key in ['title', 'body', 'patient_id', 'staff_id']):
+            return 400
+    
+    else:
+        return 404
+    
+    return 200
+
 @app.route('/<path:path>',methods=['GET'])
 def proxy_get(path):
 
@@ -108,6 +171,47 @@ def proxy_delete(path):
 
     # makes get request to site
     resp = requests.delete(f'{SITE_NAME}{path}', headers={'Authorization': auth})
+
+    # adds headers to the response (excluding specified)
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+
+    # create response object
+    response = Response(resp.content, resp.status_code, headers)
+
+    return response # sends response to user
+
+@app.route('/<path:path>',methods=['POST'])
+def proxy_post(path):
+
+    # get headers
+    auth = request.headers.get('Authorization')
+    content_type = request.headers.get('Content-Type')
+
+    # get data
+    data = request.data
+
+    # validate headers
+    if not auth:
+        return Response('No Authorization header', 401)
+    if content_type != 'application/json':
+        return Response('Invalid Content-Type', 406)
+    
+    # validate post path
+    if not validate_post_path(path):
+        return Response('Invalid path', 404)
+    
+    # validate post content
+    status = validate_post_content(data, path)
+    if status == 400:
+        return Response('Invalid content', 400)
+    elif status == 404:
+        return Response('Invalid request', 404)
+    elif status == 406:
+        return Response('Invalid Content-Type', 406)
+
+    # makes get request to site
+    resp = requests.post(f'{SITE_NAME}{path}', headers={'Authorization': auth, 'Content-Type': content_type}, data=data)
 
     # adds headers to the response (excluding specified)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
