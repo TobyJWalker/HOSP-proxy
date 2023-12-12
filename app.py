e5e0c1f19de1ed9e4b1af0ab07bf0245045b0f02
TobyJWalker <tobyjwalker03@outlook.com>
2023-12-12 16:55:37 +0000

    path request handler

--- app.py
+++ app.py
@@ -130,6 +130,59 @@ def validate_post_content(content, path):
     
     return 200
 
+# validate the Post method path
+def validate_patch_path(path):
+    # split path
+    path = path.split('/')
+
+    # check if length is not 2
+    if len(path) != 2:
+        return False
+    
+    # check if first part is valid category
+    if path[0] not in VALID['CATEGORIES']:
+        return False
+    
+    if not path[1].isdigit():
+        return False
+    
+    return True
+
+# validate post content
+def validate_patch_content(content, path):
+    # check if content is json
+    try:
+        data = json.loads(content)
+    except:
+        return 406
+    
+    # get the object type to make
+    path = path.split('/')
+    obj = path[0]
+
+    # check content matches object
+    if obj == 'hospitals':
+        if not 'name' in data.keys():
+            return 400
+        
+    elif obj == 'patients':
+        if len(path) == 1:
+            if not all(key in ['name', 'hospital_id', 'birthdate'] for key in data.keys()):
+                return 400
+    
+    elif obj == 'staffs':
+        if not all(key in ['name', 'hospital_id', 'password'] for key in data.keys()):
+            return 400
+    
+    elif obj == 'notes':
+        if not all(key in ['title', 'body', 'patient_id', 'staff_id'] for key in data.keys()):
+            return 400
+    
+    else:
+        return 404
+    
+    return 200
+
 @app.route('/<path:path>',methods=['GET'])
 def proxy_get(path):
 
@@ -221,6 +274,49 @@ def proxy_post(path):
     response = Response(resp.content, resp.status_code, headers)
 
     return response # sends response to user
+
+@app.route('/<path:path>',methods=['PATCH'])
+def proxy_patch(path):
+
+    # get headers
+    auth = request.headers.get('Authorization')
+    content_type = request.headers.get('Content-Type')
+
+    # get data
+    data = request.data
+
+    # validate headers
+    if not auth:
+        return Response('No Authorization header', 401)
+    if content_type != 'application/json':
+        return Response('Invalid Content-Type', 406)
+    
+    # validate post path
+    if not validate_patch_path(path):
+        return Response('Invalid path', 404)
+    
+    # validate post content
+    status = validate_patch_content(data, path)
+    if status == 400:
+        return Response('Invalid content', 400)
+    elif status == 404:
+        return Response('Invalid request', 404)
+    elif status == 406:
+        return Response('Invalid Content-Type', 406)
+
+    # makes get request to site
+    resp = requests.patch(f'{SITE_NAME}{path}', headers={'Authorization': auth, 'Content-Type': content_type}, data=data)
+
+    # adds headers to the response (excluding specified)
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+
+    # create response object
+    response = Response(resp.content, resp.status_code, headers)
+
+    return response # sends response to user
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
